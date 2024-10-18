8fb9593fb507ee5f6b215a1c6f6abb4b5ba64817
Hyan Ferraz <hyanfr01@gmail.com>
2024-10-18 10:30:58 -0300

    fix : add return typing to UserService's functions

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './infra/schemas/User';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Document, Model } from 'mongoose';
 import { ProductDto } from './dto/Product.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async getUser(id: string) {
+  async getUser(id: string): Promise<Model<User>> {
     return await this.userModel.findById(id);
   }
 
-  async addProductToUser(userId: string, product: ProductDto) {
+  async addProductToUser(
+    userId: string,
+    product: ProductDto,
+  ): Promise<Document> {
     const user = await this.userModel.findById(userId);
     user.products.push(product);
     await this.userModel.replaceOne({ _id: userId }, user);
     return user;
   }
 
-  async removeProductFromUser(userId: string, productId: string) {
+  async removeProductFromUser(
+    userId: string,
+    productId: string,
+  ): Promise<Document> {
     const user = await this.userModel.findById(userId);
     user.products = user.products.filter((product) => product.id !== productId);
     await this.userModel.replaceOne({ _id: userId }, user);
